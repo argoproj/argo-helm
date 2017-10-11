5456fd1e8b0b0c794e62189e8566ed39cf3df01b
Alexander Matyushentsev <alexander@applatix.com>
2017-10-11 13:25:53 -0700

    Fix chart tar command

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,6 +8,6 @@ for dir in $SRCROOT/charts/*;
 do
  echo "Processing $dir"
  version=$(cat $dir/Chart.yaml | grep version: | awk '{print $2}')
- tar -cvzf $SRCROOT/output/$(basename $dir)-$version.tgz $dir -C $dir .
+ tar -cvzf $SRCROOT/output/$(basename $dir)-$version.tgz -C $dir .
  cd $SRCROOT/output && helm repo index .
 done
