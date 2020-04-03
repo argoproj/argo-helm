6211da2c66ac72b67ee0072285659a6afcd87db7
Carlos Juan Gómez Peñalver <carlosjuangp@gmail.com>
2020-04-04 00:19:16 +0100

    fix: lint and publish scripts
    Signed-off-by: Carlos Juan Gómez Peñalver <carlosjuangp@gmail.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,8 +8,19 @@ rm -rf $SRCROOT/output && git clone -b gh-pages git@github.com:argoproj/argo-hel
 
 for dir in $(find $SRCROOT/charts -mindepth 1 -maxdepth 1 -type d);
 do
+    sudo rm -rf $dir/charts
+
+    name=$(basename $dir)
+
     if [ $(helm dep list $dir 2>/dev/null| wc -l) -gt 1 ]
     then
+        # Bug with Helm subcharts with hyphen on them
+        # https://github.com/argoproj/argo-helm/pull/270#issuecomment-608695684
+        if [ "$name" == "argo-cd" ]
+        then
+            echo "Restore ArgoCD RedisHA subchart"
+            git checkout $dir
+        fi
         echo "Processing chart dependencies"
         helm --debug dep build $dir
     fi
