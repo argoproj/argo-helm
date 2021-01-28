54d7e73729d3d8094402da053874c3ae65a0907b
Josh Soref <jsoref@users.noreply.github.com>
2021-01-28 18:38:56 -0500

    feat: migrate from circle-ci to github-actions
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,8 +4,6 @@ set -eux
 SRCROOT="$(cd "$(dirname "$0")/.." && pwd)"
 GIT_PUSH=${GIT_PUSH:-false}
 
-rm -rf $SRCROOT/output && git clone -b gh-pages git@github.com:argoproj/argo-helm.git $SRCROOT/output
-
 helm repo add argoproj https://argoproj.github.io/argo-helm
 
 for dir in $(find $SRCROOT/charts -mindepth 1 -maxdepth 1 -type d);
@@ -31,12 +29,14 @@ do
     helm --debug package $dir
 done
 
-cp $SRCROOT/*.tgz output/
-cd $SRCROOT/output && helm repo index .
+cp $SRCROOT/*.tgz $SRCROOT/output/
+cd $SRCROOT/output
+helm repo index .
 
-cd $SRCROOT/output && git status
+git status
+git add . && git commit -m "Publish charts"
 
 if [ "$GIT_PUSH" == "true" ]
 then
-    cd $SRCROOT/output && git add . && git commit -m "Publish charts" && git push git@github.com:argoproj/argo-helm.git gh-pages
+    git push origin gh-pages
 fi
