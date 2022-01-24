cc75a8bfce6dcf37008caacf9dd8629dcd33ff3f
Oliver Bähler <oliverbaehler@hotmail.com>
2022-01-24 17:44:05 +0100

    feat(all): Add Documentation Guidelines
    Signed-off-by: Oliver Bähler <oliverbaehler@hotmail.com>

---
+++ scripts/helm-docs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## Reference: https://github.com/norwoodj/helm-docs
+set -eux
+CHART_DIR="$(cd "$(dirname "$0")/.." && pwd)"
+echo "$CHART_DIR"
+
+echo "Running Helm-Docs"
+docker run \
+    -v "$CHART_DIR:/helm-docs" \
+    -u $(id -u) \
+    jnorwood/helm-docs:latest
