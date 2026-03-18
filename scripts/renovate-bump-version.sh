cdda6f8e529234966b6ebcad3bfeb3e36a82a3be
Tim Collins <45351296+tico24@users.noreply.github.com>
2026-03-18 15:19:22 +0000

    feat(argo-rollouts): Script to update the Argo Rollouts CRDs (#3783)
    Signed-off-by: Tim Collins <tim@thecollins.team>
    Signed-off-by: Tim Collins <45351296+tico24@users.noreply.github.com>
    Co-authored-by: Aikawa <yu.croco@gmail.com>

--- scripts/renovate-bump-version.sh
+++ scripts/renovate-bump-version.sh
@@ -45,6 +45,9 @@ if [[ "$dependency_name" == "argo-workflows" ]]; then
   "$(dirname "$0")/update-argo-workflows-crds.sh" "$dependency_version"
 fi
 
+# If the dependency is argo-rollouts, also update CRDs
+if [[ "$dependency_name" == "argo-rollouts" ]]; then
+  "$(dirname "$0")/update-argo-rollouts-crds.sh" "$dependency_version"
 # If the dependency is argo-cd, also update CRDs
 if [[ "$dependency_name" == "argo-cd" ]]; then
   "$(dirname "$0")/update-argo-cd-crds.sh" "$dependency_version"
